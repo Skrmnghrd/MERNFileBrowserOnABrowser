0fbbe0a4f6a7978963aac435e21fe7da59db14fb
Skrmnghrd <paulogrian@gmail.com>
2020-06-08 01:40:03 -0500

    fixed the stringify and made the dirdetails accept parameters

--- backend/routes/dirDetails.js
+++ backend/routes/dirDetails.js
@@ -1,34 +1,48 @@
 const router = require('express').Router();
 const { spawn } = require("child_process");
-//let means we can only use it in this file or block, no where else
-//import this Exercise object from schema
 
 
+// main route for directory details
+// usage: post a json with this format
+// { "directory" : 'your dir here' }
+//
 
-//begin Add route, #to add a new user
-router.route('/').get((req, res) => {
+router.route('/').post((req, res) => {
         //use spawn for alot of outputs
+    console.log(req.body);
+
     var return_me = { "data" : "xdata"};
-    /* 
-    https://stackoverflow.com/questions/35318442/how-to-pass-parameter-to-a-promise-function
+
+    let promiseDirectoryDetails = function(directory) {
+        return new Promise(function(resolve, reject){
+
+            //convert dir to string for sec reasons
+            let ls = spawn("ls", [String(directory)]); 
+            ls.stdout.on("data", data => {
+                //from a buffer, to string, to an array split by newline
+                data = data.toString().split("\n");
+
+                //lazy way to remove last array item :) sorry
+                data.pop();
+                
+                //stringify to add "" and parse to parse it neatly
+                return_me.data = JSON.parse(JSON.stringify(data) );
+                //console.log(return_me.data );
+
+                resolve("Promise DirDet: Success!"); 
+                // call this or things won't stop looping here
+
+                //add reject in the future
+            });
+        });
     
-    */
-    let promiseDirectoryDetails = new Promise(function(err, data){
-        
-        let ls = spawn("ls", ['.']); //. for now, change later
-
-        ls.stdout.on("data", data => {
-            return_me.data = data;
-            console.log('IT WORKS');
-            resolve({ msg: 'It works' });
-        })
-    })
-
-    promiseDirectoryDetails.then((result) => {
-        //console.log(return_me.data.toString());
-        res.json(return_me.data.toString());
-    }).catch( (error) =>{
-        console.log("Error", error );
+    }
+    
+    promiseDirectoryDetails(req.body.directory).then((resolve) => {
+        console.log(resolve);
+        res.json(return_me.data);
+    }).catch( (reject) =>{
+        console.log("Error", reject );
     })
 });
 
